02762795e7335646b820467a4cf0b9f9f4760201
David Schmidt <david.schmidt@rwth-aachen.de>
2018-03-17 13:09:46 +0100

    Add task to create validation plots and ratios for trigger weights.

--- workflow/tasks/T030_triggerWeightsPlots.py
+++ workflow/tasks/T030_triggerWeightsPlots.py
@@ -2,12 +2,9 @@
 
 import utils.email
 from workflow.base_tasks.base import Task
-from workflow.enums import LeptonType, Tagger
-from workflow.legacy_tasks.rootScriptRunner import RootScriptRunner
+from workflow.legacy_tasks.checkVar import CheckVar
 from workflow.tasks.T020_triggerWeights import TriggerWeights
-
-import luigi
-import os
+from workflow.enums import LeptonType
 
 # Make plots with checkVar:
 # -This produces plots from the root files made from reweighting
@@ -66,33 +63,6 @@ class TriggerWeightsPlots(Task):
                     ratios[leptonType.name][hf_str] = float(f.readline())
         output.dump(ratios)
         utils.email.notify('TriggerWeightsPlots done')
-
-
-class CheckVar(RootScriptRunner):
-    tagger = luigi.EnumParameter(
-        enum=Tagger,
-        default=Tagger.csv,
-    )
-    isHF = luigi.BoolParameter(
-        default=True,
-    )
-    versionNum = luigi.IntParameter(
-        default=0,
-    )
-    leptonType = luigi.EnumParameter(enum=LeptonType)
-
-    @property
-    def list_scripts(self):
-        result = [
-            "root_legacy/csvReweightingRun2/csvTreeMaker/"
-            "CSVHistoFiles/checkVar.C"
-            "'(%d, %d, \"%s/\", \"%s/\", \"_v%d_histo_All.root\", \"%s\")'"
-            % (self.tagger.value, self.isHF,
-               os.path.dirname(self.output()['png'][0].path),
-               os.path.dirname(self.input()[0].path), self.versionNum,
-               self.leptonType.name)
-        ]
-        return result
 
 
 class CheckVarTriggerWeights(CheckVar):
--- workflow/tasks/T040_applyTriggerWeights.py
+++ workflow/tasks/T040_applyTriggerWeights.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import utils.email
 from workflow.base_tasks.base import Task
 from workflow.enums import LeptonType, FinalState
 from workflow.legacy_tasks.csvSF_TreeReader import CSVSF_TreeReader
+from workflow.legacy_tasks.checkVar import CheckVar
 
 import law
 
@@ -17,11 +19,14 @@ import law
 # -Change .root file suffix (L104 & L114)
 # -Uncomment all fileData (L115 & L170)
 # -Make it uses all 3 added together (L174-L178)
+#
+# -Then make lf & hf plots and post
+#     root -l checkVar.C'(cMVA(0)/csv(1), LF(0)/HF(1), "<suffix>")'
 
 
 class ApplyTriggerWeights(Task, law.WrapperTask):
     inclusiveSelection = True
-    useTriggerWeights = True
+    useTriggerWeights = True    # TODO right now this just uses the already hard coded trigger weights
 
     def requires(self):
         requs = []
@@ -38,8 +43,6 @@ class ApplyTriggerWeights(Task, law.WrapperTask):
             for leptonType in LeptonType:
                 if leptonType == LeptonType.All:
                     continue
-                if leptonType == LeptonType.MuonEG and not hf:
-                    continue
                 requs.append(CSVSF_TreeReader(
                     useTriggerWeights=self.useTriggerWeights,
                     isHF=hf,
@@ -52,3 +55,72 @@ class ApplyTriggerWeights(Task, law.WrapperTask):
 
     def output(self):
         return self.input()
+
+
+class AppliedTriggerWeightsPlots(Task):
+    def requires(self):
+        requs = dict()
+        for hf, hf_str in zip([True, False], ['hf', 'lf']):
+            requs[hf_str] = CheckVarAppliedTriggerWeights(
+                isHF=hf,
+                version=self.version
+            )
+        return requs
+
+    def output(self):
+        return self.local_target('dataMCratio.json')
+
+    def run(self):
+        output = self.output()
+        output.parent.touch()
+        ratios = dict()
+        for hf, hf_str in zip([True, False], ['hf', 'lf']):
+            with open(self.input()[hf_str]['ratio'].path,
+                      'r') as f:
+                ratios[hf_str] = float(f.readline())
+        output.dump(ratios)
+        utils.email.notify('AppliedTriggerWeightsPlots done')
+
+
+class CheckVarAppliedTriggerWeights(CheckVar):
+    var_list = [
+        'probe_jet_csv',
+        'nJets',
+        'nTags',
+        'all_jet_csv',
+        'probe_jet_pt',
+        'all_jet_pt',
+        'first_jet_pt',
+        'first_jet_eta',
+        'first_jet_csv',
+        'first_jet_flavour',
+        'second_jet_pt',
+        'second_jet_eta',
+        'second_jet_csv',
+        'second_jet_flavour',
+        'met_pt',
+        'mht_pt',
+        'dr_leplep',
+        'mass_leplep',
+        '1stlep_pt',
+        '2ndlep_pt',
+        'lep_pt',
+    ]
+    leptonType = LeptonType.All
+
+    def requires(self):
+        return ApplyTriggerWeights(version=self.version)
+
+    def output(self):
+        flavor_str = 'lf'
+        if self.isHF:
+            flavor_str = 'hf'
+
+        result = []
+        for varName in self.var_list:
+            fname = '%s_%s_%s.png' % (self.tagger.name, varName, flavor_str)
+            result.append(self.local_target(self.leptonType.name, fname))
+        result = {'png': result}
+        result['ratio'] = self.local_target(self.leptonType.name,
+                                            flavor_str + '_dataMCratio.txt')
+        return result

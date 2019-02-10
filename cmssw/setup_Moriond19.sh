940558c41d92be255c345382db3f1c766b392544
Yannik Rath <yannik.alexander.rath@cern.ch>
2019-02-10 21:50:13 +0100

    Add cmssw setup file for 10_2_X.

---
+++ cmssw/setup_Moriond19.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env
+
+action() {
+    local origin="$( pwd )"
+    local scram_cores="$SCRAM_CORES"
+    [ -z "$scram_cores" ] && scram_cores="1"
+
+    export SCRAM_ARCH="slc6_amd64_gcc700"
+    export CMSSW_VERSION="CMSSW_10_2_11"
+    export CMSSW_BASE="$JTSF_DATA/cmssw/$CMSSW_VERSION"
+
+    source "/cvmfs/cms.cern.ch/cmsset_default.sh"
+
+    if [ ! -d "$CMSSW_BASE" ]; then
+        mkdir -p "$( dirname "$CMSSW_BASE" )"
+        cd "$( dirname "$CMSSW_BASE" )"
+        scramv1 project CMSSW "$CMSSW_VERSION"
+        cd "$CMSSW_VERSION/src"
+        eval `scramv1 runtime -sh`
+        scram b
+
+        #
+        # custom topics
+        #
+
+        git cms-merge-topic cms-egamma:EgammaPostRecoTools
+        git cms-addpkg RecoBTag/TensorFlow
+        git cherry-pick 94ceae257f846998c357fcad408986cc8a039152
+
+        scram b -j "$scram_cores"
+
+    else
+        cd "$CMSSW_BASE/src"
+        eval `scramv1 runtime -sh`
+    fi
+
+    cd "$origin"
+    export PYTHONPATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc700/lcg/root/6.12.07-gnimlf5/lib:$PYTHONPATH
+}
+action "$@"

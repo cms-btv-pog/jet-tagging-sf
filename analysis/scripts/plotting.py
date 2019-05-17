6a95a3fbd9419c4b65d58869005d9847518b163e
Yannik Rath <yannik.alexander.rath@cern.ch>
2019-05-17 16:24:25 +0200

    Update script to plot from BTV group csv files.

--- analysis/scripts/plotting.py
+++ analysis/scripts/plotting.py
@@ -17,24 +17,19 @@ import tarfile
 import luigi
 
 from analysis.tasks.base import AnalysisTask
-from analysis.tasks.plotting import PlotTask
-from analysis.tasks.measurement import FitScaleFactors
-from analysis.tasks.hists import MergeHistograms
 from analysis.root import ROOTPlot
-from analysis.config.jet_tagging_sf import binning_to_selection
 from law.target.local import LocalDirectoryTarget
 
 dirname = os.path.abspath(os.path.dirname(__file__))
 
-class PlotFromCSV(PlotTask):
-    shift = FitScaleFactors.shift
-    iteration = FitScaleFactors.iteration
-    fix_normalization = FitScaleFactors.fix_normalization
-    inputFile = os.path.join(dirname, "DeepCSV_94XSF_V3_B_F.csv")
+class PlotFromCSV(AnalysisTask):
+    shift = luigi.Parameter()
+    flavor = luigi.ChoiceParameter(choices=["lf", "c", "hf"])
+    csv_file = os.path.join(dirname, "DeepCSV_94XSF_V3_B_F.csv")
+    compare_file = os.path.join(dirname, "DeepCSV_94XSF_V4_B_F.csv")
 
-    def requires(self):
-        return FitScaleFactors.req(self, version=self.get_version(FitScaleFactors),
-                 _prefer_cli=["version"])
+    def output(self):
+        return self.local_target("plots.tgz")
 
     def run(self):
         ROOT.PyConfig.IgnoreCommandLineOptions = True
@@ -43,87 +38,64 @@ class PlotFromCSV(PlotTask):
         ROOT.gSystem.Load('libCondFormatsBTauObjects')
         ROOT.gSystem.Load('libCondToolsBTau')
 
-        inp = self.input()
-        outp = self.output()
         local_tmp = LocalDirectoryTarget(is_tmp=True)
         local_tmp.touch()
 
-        calib = ROOT.BTagCalibration("csvv1", self.inputFile)
-
-        shift, direction = self.shift.rsplit("_", 1)
-        sysType = direction + "_" + shift # TODO: Different shift name mappings
-
         v_sys = getattr(ROOT, 'vector<string>')()
-        v_sys.push_back(sysType)
+        v_sys.push_back("up_" + self.shift)
+        v_sys.push_back("down_" + self.shift)
 
-        reader = ROOT.BTagCalibrationReader(
-            3,              # 0 is for loose op, 1: medium, 2: tight, 3: discr. reshaping
-            "central",      # central systematic type
-            v_sys,          # vector of other sys. types
-        )
-
-        for jetFlavor in [0, 1, 2]:
-            reader.load(
-                calib,
-                jetFlavor,          # 0 is for b flavour, 1: FLAV_C, 2: FLAV_UDSG
-                "iterativefit"      # measurement type
+        readers = {}
+        for input_file, id in zip([self.csv_file, self.compare_file], ["first", "compare"]):
+            calib = ROOT.BTagCalibration("csvv1", input_file)
+            reader = ROOT.BTagCalibrationReader(
+                3,              # 0 is for loose op, 1: medium, 2: tight, 3: discr. reshaping
+                "central",      # central systematic type
+                v_sys,          # vector of other sys. types
             )
 
-        flavors = {"lf": 2, "c": 1, "hf": 0}
-        binning = self.config_inst.get_aux("binning")
+            for jetFlavor in [0, 1, 2]:
+                reader.load(
+                    calib,
+                    jetFlavor,          # 0 is for b flavour, 1: FLAV_C, 2: FLAV_UDSG
+                    "iterativefit"      # measurement type
+                )
+            readers[id] = reader
 
-        with inp["sf"].load("r") as input_file:
-            for flavorName, flavorID in flavors.items():
-                flavor_bins = binning[flavorName]
-                csv_bins = flavor_bins["deepcsv"]["measurement"]
+        flavor_ids = self.config_inst.get_aux("flavor_ids")
+        binning = self.config_inst.get_aux("binning")[self.flavor]
 
-                pt_binning = [(start, end) for start, end in zip(flavor_bins["pt"][:-1], flavor_bins["pt"][1:])]
-                eta_binning = [(start, end) for start, end in zip(flavor_bins["abs(eta)"][:-1], flavor_bins["abs(eta)"][1:])]
+        pt_binning = [(start, end) for start, end in zip(binning["pt"][:-1], binning["pt"][1:])]
+        eta_binning = [(start, end) for start, end in zip(binning["abs(eta)"][:-1], binning["abs(eta)"][1:])]
 
-                csv_min = min(csv_bins)
-                csv_max = max(csv_bins)
+        for pt_idx, pt_range in enumerate(pt_binning):
+            for eta_idx, eta_range in enumerate(eta_binning):
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.set_title("pt: %s to %s, eta: %.1f to %.1f" % (pt_range + eta_range))
 
-                for eta_range, pt_range in itertools.product(eta_binning, pt_binning):
-                    # get new scale factors
-                    pt_selection = binning_to_selection(pt_range, "pt")[0][0]
-                    eta_selection = binning_to_selection(eta_range, "eta")[0][0]
-                    category = "measure__{}__pt{}__eta{}".format(flavorName, pt_selection, eta_selection)
-                    category_dir = input_file.Get(category)
-                    hist = category_dir.Get("sf")
+                pt_val = np.mean(pt_range)
+                eta_val = np.mean(eta_range)
 
-                    fig = plt.figure()
-                    ax = fig.add_subplot(111)
-                    ax.set_title("pt: %s to %s, eta: %.1f to %.1f" % (pt_range + eta_range))
+                for sys_type in ["central", "up_" + self.shift, "down_" + self.shift]:
+                    for id, reader in readers.items():
+                        x_values = np.linspace(0., 1., 10000)
+                        y_values = []
+                        for csv_value in x_values:
+                            sf = reader.eval_auto_bounds(
+                                sys_type,      # systematic (here also 'up'/'down' possible)
+                                flavor_ids[self.flavor],              # jet flavor
+                                eta_val,            # absolute value of eta
+                                pt_val,             # pt
+                                csv_value
+                            )
+                            y_values.append(sf)
+                        ax.plot(x_values, y_values, label="{}, {}".format(id, sys_type))
 
-                    eta_val = sum(eta_range) / 2.
-                    pt_val = sum(pt_range) / 2.
+                ax.legend()
+                fig.savefig(os.path.join(local_tmp.path, "SF_%s_%s_Pt%sto%s_eta%.1fTo%.1f.pdf" % ((self.flavor, self.shift) + pt_range + eta_range)))
 
-                    # get values for previous scale factors
-                    x_values = np.linspace(csv_min, csv_max, 10000)
-                    y_values = []
-                    for csv_value in x_values:
-                        sf = reader.eval_auto_bounds(
-                            sysType,      # systematic (here also 'up'/'down' possible)
-                            flavorID,              # jet flavor
-                            eta_val,            # absolute value of eta
-                            pt_val,             # pt
-                            csv_value
-                        )
-                        y_values.append(sf)
-                    ax.plot(x_values, y_values, label="previous")
-
-                    # get values for new scale factors
-                    x_values = []
-                    y_values = []
-                    for bin_idx in range(1, hist.GetNbinsX() + 1):
-                        x_values.append(hist.GetBinCenter(bin_idx))
-                        y_values.append(hist.GetBinContent(bin_idx))
-                    ax.plot(x_values, y_values, label="new")
-
-                    ax.legend()
-                    fig.savefig(os.path.join(local_tmp.path, "SF_%s_%s_Pt%sto%s_eta%.1fTo%.1f.pdf" % ((flavorName, self.shift) + pt_range + eta_range)))
-
-        with outp.localize("w") as tmp:
+        with self.output().localize("w") as tmp:
             with tarfile.open(tmp.path, "w:gz") as tar:
                 for plot_file in os.listdir(local_tmp.path):
                     tar.add(os.path.join(local_tmp.path, plot_file), arcname=plot_file)
@@ -162,6 +134,9 @@ class PlotFromRoot(AnalysisTask):
         hist_names = [hist_tpl.format(pt_idx, eta_idx)
             for pt_idx in range(n_pt_categories) for eta_idx in range(n_eta_categories)]
 
+        ##
+        ## Plot histograms with total systematic envelope
+        ##
 
         for hist_name in hist_names:
             nominal_hist = root_file.Get(hist_name)
@@ -226,6 +201,25 @@ class PlotFromRoot(AnalysisTask):
             plot.draw({"down": combined_hist_down}, line_color=4)
 
             plot.save(os.path.join(local_tmp.path, "{}.pdf".format(hist_name)))
+
+        ##
+        ## Check scale factors, uncertainties, and fits
+        ##
+
+        for i, (pt_idx, eta_idx) in enumerate(itertools.product(range(n_pt_categories),
+            range(n_eta_categories))):
+            data_hist = root_file.Get("h_csv_Data_Pt{}_Eta{}".format(pt_idx, eta_idx))
+            if self.flavor == "hf":
+                signal_base = "h_csv_MC_bjets"
+                bg_base = "h_csv_MC_nonbjets"
+            else:
+                signal_base = "h_csv_MC_nonbjets" # should be light jets (no c), but not in file
+                bg_base = "h_csv_MC_bjets"# should be b+c jets
+
+            signal_hist = root_file.Get("{}_Pt{}_Eta{}".format(signal_base, pt_idx, eta_idx))
+            bg_hist = root_file.Get("{}_Pt{}_Eta{}".format(bg_base, pt_idx, eta_idx))
+
+            #
 
         with self.output().localize("w") as tmp:
             with tarfile.open(tmp.path, "w:gz") as tar:

43664050da5f78dec43231b75ce2f6607820b460
Yannik Rath <yannik.alexander.rath@cern.ch>
2019-06-28 12:50:59 +0200

    Singularity setup script to add missing libraries.

---
+++ singularity/setup_slc6.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/cvmfs/grid.cern.ch/Grid/dcap/2.47.10-3/x86_64-slc6-gcc44-opt/lib64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/cvmfs/grid.cern.ch/Grid/DPM/1.8.5-1/x86_64-slc6-gcc44-opt/lib64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/cvmfs/grid.cern.ch/Grid/lcg-dm-common/1.8.3.1-1/x86_64-slc6-gcc44-opt/lib64/"
+
+

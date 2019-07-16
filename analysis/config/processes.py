4d88fec2731a51cb1378c5492997056e0e451c7b
Yannik Rath <yannik.alexander.rath@cern.ch>
2019-07-16 14:51:27 +0200

    Fix process label for root plotting.

--- analysis/config/processes.py
+++ analysis/config/processes.py
@@ -149,7 +149,7 @@ process_dy_lep_5To50_Ht600ToInf = process_dy_lep_5To50.add_process(
 
 process_dy_lep_50ToInf = process_dy_lep.add_process(
     "dy_lep_50ToInf", 222,
-    label=r"Drell-Yan, $m_{ll} \gt 50$",
+    label=r"Drell-Yan, $m_{ll} > 50$",
     xsecs={
         13: sn.Number(1921.8, {
             "integration": 0.6,

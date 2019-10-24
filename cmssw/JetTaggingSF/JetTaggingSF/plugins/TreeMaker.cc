ce51e85a09c52bbf31abbc5e3c9f93e1651fd0c5
Yannik Rath <yannik.alexander.rath@cern.ch>
2019-10-24 11:32:08 +0200

    Fix variable name.

--- cmssw/JetTaggingSF/JetTaggingSF/plugins/TreeMaker.cc
+++ cmssw/JetTaggingSF/JetTaggingSF/plugins/TreeMaker.cc
@@ -411,7 +411,7 @@ void TreeMaker::setupJetCorrectionObjects()
     }
 
     // add jet variation for 2018 HEM issue
-    if (applyHEMFilter_ && !isData)
+    if (applyHEMFilter_ && !isData_)
     {
         jetVariations_.push_back(stringPair("jes" + HEMIssueSource_, "up"));
         jetVariations_.push_back(stringPair("jes" + HEMIssueSource_, "down"));
